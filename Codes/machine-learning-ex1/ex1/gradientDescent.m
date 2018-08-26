36792f9885c12428d44f46722a3801ff9eede365
kaushalvivek <vivek.kaushal@research.iiit.ac.in>
2018-08-26 18:49:59 +0530

    Gradient Descent Question

--- Codes/machine-learning-ex1/ex1/gradientDescent.m
+++ Codes/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,13 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    theta = theta - alpha*(1/m)*(X'*(X*theta-y));
+    
+    #delta=1/m*(X'*X*theta-X'*y);
+    #theta=theta-alpha.*delta;
+    
     % ============================================================
 
     % Save the cost J in every iteration    
